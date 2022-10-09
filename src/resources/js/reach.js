c740c839018c06ffdf623cc8dea6a5ec6e1acb30
meetmthosar <114148836+meetmthosar@users.noreply.github.com>
2022-10-09 14:50:19 +0530

    Reach js restored

--- src/resources/js/reach.js
+++ src/resources/js/reach.js
@@ -1,33 +1,7 @@
-console.log(11);
-// import { loadStdlib } from "@reach-sh/stdlib";
-// import { ALGO_MyAlgoConnect as MyAlgoConnect } from "@reach-sh/stdlib";
-// const reach = loadStdlib();
-//
-// reach.setWalletFallback(reach.walletFallback({
-//     providerEnv: 'TestNet', MyAlgoConnect }));
-//
-// const checkTheHead = async () => {
-//     let promiseToRobTheTime = new Promise( async (startRobbing) => {
-//
-//         startRobbing();
-//
-//     });
-//
-//     promiseToRobTheTime.then(
-//         async () => {console.log("In Promise");
-//             const acc =  await reach.getDefaultAccount(reach.parseCurrency(1000));
-//             // setAccount(acc);
-//             // await getBalance(acc);
-//             // const contrac = await acc.contract(backend, JSON.parse(contractInfo));
-//             // setContract(contrac);
-//             // // await getBalance(acc);
-//             // rob(contrac);
-//         }
-//         // () => { console.log('Error'); }
-//     ).catch(e => {
-//         console.log(e.message);
-//
-//         // if(e.message.includes('Already')) setTimeUpMessage("You have already received the token");
-//         // else setTimeUpMessage("Time up or List is full")
-//     });
-// }
+import { loadStdlib } from "@reach-sh/stdlib";
+import { ALGO_MyAlgoConnect as MyAlgoConnect } from "@reach-sh/stdlib";
+const stdlib = loadStdlib();
+
+stdlib.setWalletFallback(stdlib.walletFallback({
+    providerEnv: 'TestNet', MyAlgoConnect }));
+
